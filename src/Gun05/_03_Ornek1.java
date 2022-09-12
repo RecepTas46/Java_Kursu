dc7865951dc410151bdeae5494b9007e2dc847cd
RecepTas46 <trecep2364@gmail.com>
2022-09-12 23:55:17 +0200

    Gün105 03_Örnek1 e Scanner eklendi.

--- src/Gun05/_03_Ornek1.java
+++ src/Gun05/_03_Ornek1.java
@@ -1,10 +1,21 @@
 package Gun05;
 
+import java.util.Scanner;
+
 public class _03_Ornek1 {
     public static void main(String[] args) {
         //Bir dikdortgenin iki kenari icin uzunkuk tanimlayiniz
         //cevre ve alanini bulunuz
 
+        //bu bolümü sonradan kendim yaptim
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("Kisa kenar icin pozitif bir sayi giriniz");
+//        int kisaK= sc.nextInt();
+//        System.out.println("Uzun kenar icin pozitif bir sayi giriniz");
+//        int uzunK=sc.nextInt();
+//
+//        System.out.println("Alan="+kisaK*uzunK);
+//        System.out.println("cevre="+((kisaK*2)+(uzunK*2)));
 
         int kisaKenar=10;
         int uzunKenar=15;
